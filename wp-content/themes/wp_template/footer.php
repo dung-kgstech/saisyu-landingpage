ca446f43545ef6c23cae7979cfdb153268ed2b53
dung-kgstech <dung.hyggelig0809@gmail.com>
2025-11-14 16:55:37 +0700

    footer

--- wp-content/themes/wp_template/footer.php
+++ wp-content/themes/wp_template/footer.php
@@ -1,5 +1,65 @@
-<footer></footer>
+<footer class="footer">
+    <div class="container">
+        <div class="grid">
+
+            <div class="col">
+                <h3 class="title">Thông tin liên hệ</h3>
+                <div class="line"></div>
+
+                <p class="company">CÔNG TY TNHH KGS TECH</p>
+
+                <div class="row">
+                    <span class="icon">📞</span>
+                    <p class="label">Số Điện Thoại:</p>
+                    <a href="tel:039*****" class="value">039 *** ****</a>
+                </div>
+
+                <div class="row">
+                    <span class="icon">✉️</span>
+                    <p class="label">Email:</p>
+                    <a href="mailto:KSG_tech@mail.com" class="value">KSG_tech@mail.com</a>
+                </div>
+
+                <form action="https://fabform.io/f/{form-id}" method="post" class="form">
+                    <label for="email">Nhận thông tin từ chúng tôi</label>
+                    <input type="email" id="email" placeholder="********@*****.**" />
+                </form>
+            </div>
+
+            <div class="col">
+                <h3 class="title">Về chúng tôi</h3>
+                <div class="line"></div>
+
+                <ul class="menu">
+                    <li><a href="/">Trang chủ</a></li>
+                    <li><a href="/company">Công ty</a></li>
+                    <li><a href="/business">Dịch vụ</a></li>
+                    <li><a href="/blog">Blog</a></li>
+                </ul>
+            </div>
+
+            <div class="col">
+                <h3 class="title">Địa chỉ của chúng tôi</h3>
+                <div class="line"></div>
+
+                <iframe
+                    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d244.9415718072964!2d106.75050664459913!3d10.806322316442303!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3175266a5a5476ab%3A0x1010bc2f036b5066!2zNDAgxJDGsOG7nW5nIHPhu5EgMTYsIEFuIFBow7osIFF14bqtbiAyLCBI4buTIENow60gTWluaCwgVmlldG5hbQ!5e0!3m2!1sen!2s!4v1736471845452!5m2!1sen!2s"
+                    class="map"
+                    loading="lazy"
+                    allowfullscreen>
+                </iframe>
+            </div>
+
+        </div>
+
+        <div class="footer-bottom">
+            <p>&copy; 2024 KGS-TECH. All rights reserved.</p>
+        </div>
+    </div>
+</footer>
+
 
 <?php wp_footer(); ?>
 </body>
+
 </html>
